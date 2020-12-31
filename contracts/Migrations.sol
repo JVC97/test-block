21702c52efb179bb48dc664b1dabfccd5c9cfcd7
Tangananica-dotcom <67763673+Tangananica-dotcom@users.noreply.github.com>
2020-12-31 17:34:06 -0300

    hygtfhgb

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,37 +1,49 @@
 pragma solidity >=0.4.24;
 
 contract UserContract {
+    
+    struct Estado{
+        string estado;
+        string fecha;
+        string razon;
+    }
+    address public owner;
+
     struct User{
         string email;
         string carrera;
         string telefono;
         string estado;
         string mac;
+        bool joined;
     }
-    struct Estado{
-        string estado;
-        string fecha;
+    struct Contact{
+        string correo;
+        address contact;
     }
 
     mapping(address => User) private users;
     mapping(address => bool) private joinedUsers;
     mapping(address => Estado[]) private estados;
-    address[] total;
+    mapping(address => Contact) private contactos;
+    //Estado[] estados;
 
     function join(string email, string carrera, string telefono, string estado, string mac) public{
         require(!userJoined(msg.sender));
         User storage user = users[msg.sender];
-        user.email=email;
-        user.carrera=carrera;
-        user.telefono=telefono;
-        user.estado=estado;
-        user.mac=mac;
-
+        user.email= email;
+        user.carrera= carrera;
+        user.telefono= telefono;
+        
+        user.mac= mac;
+        estados[msg.sender];
+        User(email, carrera, telefono, estado, mac);
         joinedUsers[msg.sender]= true;
         total.push(msg.sender);
 
     }
-    function update(string email, string carrera, string telefono, string estado, string mac) public{
+
+    function update(string email, string carrera, string telefono, string  estado, string  mac) public{
         require(userJoined(msg.sender));
         User storage user = users[msg.sender];
         if(keccak256(email) != keccak256("")){
@@ -51,15 +63,37 @@ contract UserContract {
         }
     }
 
-    function getUser(address addr) public view returns (string,string,string,string,string){
+    function getUser(address addr) public view returns (string , string, string, string, string){
         require(userJoined(msg.sender));
         User memory user = users[addr];
         return(user.email, user.telefono, user.carrera, user.estado, user.mac);
-
     }
 
+    function updateState(string memory updt, string memory date, string memory razon) public{
+        address addr = msg.sender;
+        require(userJoined(addr));
+        Estado memory st=Estado({estado: updt,fecha: date, razon: razon});
+        estados[addr].push(st);
+        User memory user = users[addr];
+        user.estado= st;
+        if(updt != "libre"){
+            /* if(updt =="contagiado"){
+                for(users as u){
+                    if(u.estado != "contagiado")
+                        users[contactos[1].contact].estado = "riesgo";
+                        updateState2(address, "riesgo", "fecha", "contacto");// mandar correos
+                }
+                return null;
+            } */
+            //aux = contact(contactos);
+            return contactos;
+        }
+    }
     function userJoined(address addr) private view returns(bool){
         return joinedUsers[addr];
+    }
+    function contacts(address contact){
+        //holi
     }
     function totalUsers() public view returns (uint){
         return total.length;
